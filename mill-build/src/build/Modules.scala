61395fe3e4cfadcec89c2c3f9a94089a03545581
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2026-06-24 09:53:58 -0500

    build: use Kyo RC4 snapshot
    Pin Kyo dependencies and kyo-test wiring to 1.0.0-RC4+40-1844110f-SNAPSHOT. Move Mill and CI to Java 25 for the snapshot classfile level and upgrade Ox to 1.0.5 for Java 25 StructuredTaskScope compatibility.

--- mill-build/src/build/Modules.scala
+++ mill-build/src/build/Modules.scala
@@ -5,7 +5,14 @@ import mill.scalalib.*
 import mill.scalajslib.*
 import io.eleven19.kymora.kyo.mill.test as kymoraTest
 
+object Versions:
+  val kyo = "1.0.0-RC4+40-1844110f-SNAPSHOT"
+
 trait CommonScalaModule extends ScalaModule with scalafmt.ScalafmtModule {
+  override def repositories = Task {
+    super.repositories() ++ Seq("https://central.sonatype.com/repository/maven-snapshots/")
+  }
+
   override def scalaVersion = Task {
     "3.8.4"
   }
@@ -23,15 +30,29 @@ trait CommonScalaModule extends ScalaModule with scalafmt.ScalafmtModule {
   }
 }
 
-trait CommonScalaTestModule extends ScalaModule with scalafmt.ScalafmtModule
+trait CommonScalaTestModule extends ScalaModule with scalafmt.ScalafmtModule {
+  override def repositories = Task {
+    super.repositories() ++ Seq("https://central.sonatype.com/repository/maven-snapshots/")
+  }
+}
 
-trait KyoTestModule extends kymoraTest.KyoTestModule
+trait KyoTestModule extends kymoraTest.KyoTestModule:
+  override def kyoVersion = Task {
+    Versions.kyo
+  }
 
 trait CommonScalaJSModule extends ScalaJSModule with scalafmt.ScalafmtModule {
+  override def repositories = Task {
+    super.repositories() ++ Seq("https://central.sonatype.com/repository/maven-snapshots/")
+  }
+
   def scalaJSVersion = "1.22.0"
 }
 
-trait KyoJSTestModule extends kymoraTest.KyoTestJSModule
+trait KyoJSTestModule extends kymoraTest.KyoTestJSModule:
+  override def kyoVersion = Task {
+    Versions.kyo
+  }
 
 /** Groups `ascribe/pipeline/{core,html,...}` so Mill discovers `ascribe.pipeline.*` children. */
 trait PipelineContainerModule extends Module
